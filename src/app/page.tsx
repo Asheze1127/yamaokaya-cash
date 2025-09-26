9bc6f8aef811ce8596642e29a9bcd2f36f6fc294
Asheze1127 <m.k.h11272005@gmail.com>
2025-09-26 23:51:46 +0900

    login signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import LoginContainer from '@/components/login/loginContainer';
+import { useState } from 'react';
 
 export default function Home() {
+  const [signIn, setSignIn] = useState(false);
   return (
-    {
-      signIn?(
-
-      ):(
-        
-      )
-
-    }
+    <div>
+          <LoginContainer isSignIn={signIn} setSignIn={setSignIn}/>
+    </div>
   )
 }
--- src/components/login/loginContainer.tsx
+++ src/components/login/loginContainer.tsx
@@ -1,14 +1,23 @@
 import { Button, TextField, Typography } from "@mui/material";
 
-export default function LoginContainer() {
-  return (
-    <div>
-      <form>
-        <Typography variant="h6">ログイン</Typography>
-        <TextField label="メールアドレス" type="email" />
-        <TextField label="パスワード" type="password" />
-        <Button type="submit">ログイン</Button>
-      </form>
-    </div>
-  );
+interface LoginContainerProps {
+    isSignIn: boolean;
+    setSignIn: (isSignIn: boolean) => void;
+}
+
+export default function LoginContainer({ isSignIn, setSignIn }: LoginContainerProps) {
+    const changeSignIn = () => {
+        setSignIn(!isSignIn);
+    }
+    return (
+        <div>
+            <form>
+                <Typography variant="h6">{isSignIn ? "ログイン" : "新規登録"}</Typography>
+                <TextField label="メールアドレス" type="email" />
+                <TextField label="パスワード" type="password" />
+                <Button type="submit">{isSignIn ? "ログイン" : "新規登録"}</Button>
+                <Button type="button" onClick={() => changeSignIn()}>{isSignIn ? "新規登録はこちら" : "ログインはこちら"}</Button>
+            </form>
+        </div>
+    );
 }
